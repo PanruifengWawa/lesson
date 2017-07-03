a960fd5b173cf800ebca372a593ba7c168940534
PanruifengWawa <46937922@qq.com>
2017-07-03 10:47:47 +0800

    share book

--- src/main/java/com/lesson/controller/BookController.java
+++ src/main/java/com/lesson/controller/BookController.java
@@ -35,7 +35,8 @@ public class BookController {
 	* @api {post} api/book 添加书籍（用户）
 	* @apiName book_add
 	* @apiGroup book
-	*
+	* 
+	* @apiHeader {String} token 身份凭证
 	* @apiParam {String} bookName * 书籍名（必须）
 	* @apiParam {String} bookImgSrc * 书籍封面（必须）
 	*
@@ -93,6 +94,7 @@ public class BookController {
 	* @apiName book_get_book_list
 	* @apiGroup book
 	*
+	* @apiHeader {String} token 身份凭证
 	* @apiParam {int} numberPerPage * 每页大小（非必须,默认分页,1-10）//这是由于阅读的书籍可能过多,像1000本
 	* @apiParam {int} currentPage * 当前页（非必须）
 	*
@@ -111,10 +113,10 @@ public class BookController {
     *			"courseContentId": 2
     *		}],
   	*		"token": null,
-  	*		"numberPerPage": 0,
-	*  		"currentPage": 0,
-	*  		"totalNumber": 0,
-	*  		"totalPage": 0
+  	*		"numberPerPage": 10,
+	*  		"currentPage": 1,
+	*  		"totalNumber": 100,
+	*  		"totalPage": 10
 	*	}
 	*
 	* @apiSuccessExample {json} Error-Response:
@@ -148,7 +150,8 @@ public class BookController {
 	* @api {delete} api/book/{bookId} 删除书籍（用户,只能删除自己手动添加的）
 	* @apiName book_delete_book
 	* @apiGroup book
-	*
+	* 
+	* @apiHeader {String} token 身份凭证
 	* @apiParam {Long} bookId * 书籍id
 	*
 	* @apiSuccessExample {json} Success-Response:
@@ -188,6 +191,62 @@ public class BookController {
 		CheckUser.checkUser(token, userTypes);
 		
 		return bookService.deleteBook(token.getUserId(), bookId);
+	}
+	
+	
+	
+	/**
+	* @api {get} api/book/share 书籍分享
+	* @apiName book_share
+	* @apiGroup book
+	* 
+	* @apiParam {Long} userId * 用户id
+	* @apiParam {int} numberPerPage * 每页大小（必须,默认分页,1-10）//防止有人直接调用接口拖库
+	* @apiParam {int} currentPage * 当前页（必须）
+	*
+	* @apiSuccessExample {json} Success-Response:
+	* 	HTTP/1.1 200 ok
+	* 	{
+  	*		"callStatus": "SUCCEED",
+  	*		"errorCode": "成功",
+  	*		"data": [{
+    *			"bookId": 1,
+    *			"bookName": "测试书籍1",
+    *			"bookImgSrc": "http://www.baidu.com",
+    *			"readDate": 1498663078000,
+    *			"userId": 3,
+    *			"isCourseBook": 1,//0-个人书籍,1-课程书籍
+    *			"courseContentId": 2
+    *		}],
+  	*		"token": null,
+  	*		"numberPerPage": 10,
+	*  		"currentPage": 1,
+	*  		"totalNumber": 100,
+	*  		"totalPage": 10
+	*	}
+	*
+	* @apiSuccessExample {json} Error-Response:
+	* 	HTTP/1.1 200 ok
+	* 	{
+	*  		"callStatus": "FAILED",
+	*  		"errorCode": "权限错误",
+	*  		"data": "用户未登录",
+	*  		"token": null
+	*  		"numberPerPage": 10,
+	*  		"currentPage": 1,
+	*  		"totalNumber": 1,
+	*  		"totalPage": 1
+	*	}
+	**/
+	@RequestMapping(value = "share", method = RequestMethod.GET)
+	@ResponseBody
+	public DataWrapper<List<Book>> getUserBookList(
+			@RequestParam(value = "userId", required = true) Long userId,
+			@RequestParam(value = "numberPerPage", required = true) Integer numberPerPage,
+    		@RequestParam(value = "currentPage", required = true) Integer currentPage,
+			HttpServletRequest request
+			) {
+		return bookService.getUserBookListByUserId(userId, numberPerPage, currentPage);
 	}
 
 }
